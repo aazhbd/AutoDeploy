d59a0db56cd1908adccf8aaae5a1e4b4dbef5104
AAZ H <aazhbd@yahoo.com>
2015-10-27 02:01:55 +0100

    First commit

---
+++ data/index.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>Home page.</title>
+	</head>
+	
+	<body>
+		<p>
+			<a href="/ArtWeb/webroot/">Click here to open ArtWeb</a>
+		</p>
+		<?php
+			echo phpinfo();
+		?>
+	</body>
+</html>
+
---
+++ data/init.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+sudo -i
+export DEBIAN_FRONTEND=noninteractive
+echo "********** Updating **********"
+apt-get update
+apt-get -y upgrade
+apt-get -y dist-upgrade
+echo "********** Installing Apache2 **********"
+apt-get -y install apache2
+echo "********** Installing MySQL **********"
+apt-get -q -y install mysql-server
+echo "********** Installing PHP5 **********"
+apt-get -q -y install php5 libapache2-mod-php5 php5-gd php5-mysql
+
+echo "********** Installing ArtWeb **********"
+cd /var/www/html/
+
+if [ -d "/var/www/html/ArtWeb/" ]; then
+	echo "********** Updating the existing copy **********"
+	cd /var/www/html/ArtWeb/
+	git pull --rebase
+	php -r "readfile('https://getcomposer.org/installer');" | php
+	php composer.phar update
+fi
+
+if [ ! -d "/var/www/html/ArtWeb/" ]; then
+	echo "********** Cloning new copy **********"
+	cd /var/www/html/
+	git clone https://github.com/aazhbd/ArtWeb.git
+	cd /var/www/html/ArtWeb/
+	php -r "readfile('https://getcomposer.org/installer');" | php
+	php composer.phar install
+fi
+
+
+if ! mysql -u root -e 'use dbname'; then
+	echo "create database dbname;" | mysql -u root
+fi
+
+cat /var/www/html/logo.txt
