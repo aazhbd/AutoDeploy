b81627d37ba7636d21583a7a5d033184d0ff0921
AAZ H <aazhbd@yahoo.com>
2015-12-03 20:15:54 +0100

    Updated port forwarding, web server and module configuration.

--- data/init.sh
+++ data/init.sh
@@ -2,22 +2,27 @@
 
 sudo -i
 export DEBIAN_FRONTEND=noninteractive
-echo "********** Updating **********"
+echo "---------------------------------------- Updating ----------------------------------------"
 apt-get update
 apt-get -y upgrade
 apt-get -y dist-upgrade
-echo "********** Installing Apache2 **********"
+echo "---------------------------------------- Installing Apache2 ----------------------------------------"
 apt-get -y install apache2
-echo "********** Installing MySQL **********"
+sudo a2enmod rewrite
+service apache2 restart
+
+cp /var/www/html/apache-conf.txt /etc/apache2/sites-enabled/000-default.conf
+
+echo "---------------------------------------- Installing MySQL ----------------------------------------"
 apt-get -q -y install mysql-server
-echo "********** Installing PHP5 **********"
+echo "---------------------------------------- Installing PHP5 ----------------------------------------"
 apt-get -q -y install php5 libapache2-mod-php5 php5-gd php5-mysql
 
-echo "********** Installing ArtWeb **********"
+echo "---------------------------------------- Installing ArtWeb -----------------------------------"
 cd /var/www/html/
 
 if [ -d "/var/www/html/ArtWeb/" ]; then
-	echo "********** Updating the existing copy **********"
+	echo "----------------------------------- Updating the existing copy -----------------------------------"
 	cd /var/www/html/ArtWeb/
 	git pull --rebase
 	php -r "readfile('https://getcomposer.org/installer');" | php
@@ -25,7 +30,7 @@ if [ -d "/var/www/html/ArtWeb/" ]; then
 fi
 
 if [ ! -d "/var/www/html/ArtWeb/" ]; then
-	echo "********** Cloning new copy **********"
+	echo "----------------------------------- Cloning new copy -----------------------------------"
 	cd /var/www/html/
 	git clone https://github.com/aazhbd/ArtWeb.git
 	cd /var/www/html/ArtWeb/
@@ -36,7 +41,7 @@ fi
 
 if ! mysql -u root -e 'use dbname'; then
 	echo "create database dbname;" | mysql -u root
-	echo "********** Database created **********"
+	echo "----------------------------------- Database created -----------------------------------"
 fi
 
 service apache2 restart
