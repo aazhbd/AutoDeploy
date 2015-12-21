f23e5fea73501768c9fac0933a65ed28d93742ee
AAZ H <aazhbd@yahoo.com>
2015-12-21 19:58:18 +0000

    Modified PHP and Apache packages and configurations

--- data/init.sh
+++ data/init.sh
@@ -2,21 +2,33 @@
 
 sudo -i
 export DEBIAN_FRONTEND=noninteractive
+
 echo "---------------------------------------- Updating ----------------------------------------"
 apt-get update
 apt-get -y upgrade
 apt-get -y dist-upgrade
-echo "---------------------------------------- Installing Apache2 ----------------------------------------"
-apt-get -y install apache2
-sudo a2enmod rewrite
-service apache2 restart
 
+echo "---------------------------------------- Basic Tools ----------------------------------------"
+apt-get -y install rcconf dialog rsync curl traceroute bash-completion nmap mtr-tiny
+
+echo "---------------------------------------- Installing Apache2 ----------------------------------------"
+apt-get -y install apache2 apache2-mpm-itk
+a2enmod actions headers rewrite cgi
+a2dismod mpm_event
+a2enmod mpm_prefork
+a2enmod rewrite
 cp /var/www/html/apache-conf.txt /etc/apache2/sites-enabled/000-default.conf
 
-echo "---------------------------------------- Installing MySQL ----------------------------------------"
+service apache2 restart
+
+echo "---------------------------------------- Installing Database ----------------------------------------"
 apt-get -q -y install mysql-server
-echo "---------------------------------------- Installing PHP5 ----------------------------------------"
-apt-get -q -y install php5 libapache2-mod-php5 php5-gd php5-mysql
+apt-get -y install mysql-client-5.6 sqlite3
+
+echo "---------------------------------------- Installing Language Platforms ----------------------------------------"
+apt-get -q -y install php5 libapache2-mod-php5 php5-gd php5-{gd,mysqlnd,sqlite}
+apt-get -y install libapache2-mod-perl2
+apt-get -y install libapache2-mod-passenger ruby-rmagick
 
 echo "---------------------------------------- Installing ArtWeb -----------------------------------"
 cd /var/www/html/
@@ -24,7 +36,7 @@ cd /var/www/html/
 if [ -d "/var/www/html/ArtWeb/" ]; then
 	echo "----------------------------------- Updating the existing copy -----------------------------------"
 	cd /var/www/html/ArtWeb/
-	git pull --rebase
+	git pull origin
 	php -r "readfile('https://getcomposer.org/installer');" | php
 	php composer.phar update
 fi
